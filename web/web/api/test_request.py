962f79ef06f1886c1b77670d63e9193f573dedd9
Kirill <storm_ne@inbox.ru>
2022-06-26 19:25:21 +0300

    token fix, request fix

--- web/web/api/test_request.py
+++ web/web/api/test_request.py
@@ -8,16 +8,10 @@ def post_user():
     Запрос POST на добавление юзверя в БД.
     """
     url = 'http://127.0.0.1:8000/api/user'
-    data = {'username': 'qwe', 'first_name': '', 'last_name': '', 'user_id_tg': '1313', 'warn': '0'}
-    send = requests.post(url=url, json=data)
-    return send
-
-
-def post_faq():
-    """добавление справки"""
-    url = 'http://127.0.0.1:8000/api/faq'
-    data = {'title': 'test', 'description': 'test test'}
-    send = requests.post(url=url, json=data)
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMCwidXNlcm5hbWUiOiJzdCIsImV4cCI6MTY1ODgzMjU2OSwiZW1haWwiOiIifQ.LAYd3Y683-bJbA6OxHZp_O05JaTq1O-V8lm4ODU2dBI"
+    headers = {"Authorization": f"Bearer {token}"}
+    data = {'username': 'zxczxc', 'first_name': '', 'last_name': '', 'user_id_tg': '9999', 'warn': '0'}
+    send = requests.post(url=url, data=data, headers=headers)
     return send
 
 
@@ -28,8 +22,10 @@ def post_poll():
     Запрос GET вернет все созданные голосования, запрос GET api/poll/id голосования вернет само голосование
     """
     url = 'http://127.0.0.1:8000/api/poll'
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMCwidXNlcm5hbWUiOiJzdCIsImV4cCI6MTY1ODgzMjU2OSwiZW1haWwiOiIifQ.LAYd3Y683-bJbA6OxHZp_O05JaTq1O-V8lm4ODU2dBI"
+    headers = {"Authorization": f"Bearer {token}"}
     data = {'keyboard_id': '123', 'user_id': '112233'}
-    send = requests.post(url=url, json=data)
+    send = requests.post(url=url, data=data, headers=headers)
     return send
 
 
@@ -39,33 +35,54 @@ def post_block():
     юзверя. Если в ответ на добавление приходит "detail": "Страница не найдена" - то такого юзверя в базе нет
     """
     url = 'http://127.0.0.1:8000/api/block'
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMCwidXNlcm5hbWUiOiJzdCIsImV4cCI6MTY1ODgzMjU2OSwiZW1haWwiOiIifQ.LAYd3Y683-bJbA6OxHZp_O05JaTq1O-V8lm4ODU2dBI"
+    headers = {"Authorization": f"Bearer {token}"}
     data = {'user': '300', 'permanent': False}
-    send = requests.post(url=url, json=data)
-    print(send)
-    print(Response)
+    send = requests.post(url=url, data=data, headers=headers)
     return send
 
 def login_token():
-    data = {"username": "st", "password": "130982"}
-    url = 'http://127.0.0.1:8000/api/login/'
+    """
+    Изначально надо зайти в админку и создать там юзера для Бота.
+     Проще всего создавать юзера с username - имя бота, password - так же имя бота и использовать
+    в переменных их bot_name и bot_password. В ответ на логин придет словарь, содержащий имя бота и токен, который
+    нужно использовать при запросах на другие методы api. Слеш в конце адреса обязателен!!!
+    """
+    bot_name = 'st'
+    bot_password = '130982'
+    data = {'username': f'{bot_name}', 'password': f'{bot_password}'}
+    url = 'http://127.0.0.1:8000/api/login/'  # косая черта в запросе на логин в конце обязательна!
     send = requests.post(url=url, data=data)
-    print(send.json()['token'])
+    print(Response.getvalue(self=send))
     return send
 
 def faq_list():
     """
-    Не работает. Пытаюсь понять как делают запросы с токеном
     Тестируем получение справки с авторизацией"""
-    url = 'http://127.0.0.1:8000/api/faq'
-    token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMCwidXNlcm5hbWUiOiJzdCIsImV4cCI6MTY1ODgzMjU2OSwiZW1haWwiOiIifQ.LAYd3Y683-bJbA6OxHZp_O05JaTq1O-V8lm4ODU2dBI'
-    headers = {'Authorization:' 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMCwidXNlcm5hbWUiOiJzdCIsImV4cCI6MTY1ODgzMjU2OSwiZW1haWwiOiIifQ.LAYd3Y683-bJbA6OxHZp_O05JaTq1O-V8lm4ODU2dBI'}
-    send = requests.post(url=url, headers=headers)
-    print(Response)
+    url = "http://127.0.0.1:8000/api/faq"
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMCwidXNlcm5hbWUiOiJzdCIsImV4cCI6MTY1ODgzMjU2OSwiZW1haWwiOiIifQ.LAYd3Y683-bJbA6OxHZp_O05JaTq1O-V8lm4ODU2dBI"
+    headers = {"Authorization": f"Bearer {token}"}
+    send = requests.get(url=url, headers=headers)
+    print(Response.getvalue(self=send))
     return send
+
+def faq_add():
+    """
+    Добавление справки в БД. Остальные методы по аналогии без data
+    """
+    url = "http://127.0.0.1:8000/api/faq"
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMCwidXNlcm5hbWUiOiJzdCIsImV4cCI6MTY1ODgzMjU2OSwiZW1haWwiOiIifQ.LAYd3Y683-bJbA6OxHZp_O05JaTq1O-V8lm4ODU2dBI"
+    headers = {"Authorization": f"Bearer {token}"}
+    data = {"title": "test2", "description": "test2"}
+    send = requests.post(url=url, data=data, headers=headers)
+    print(Response.getvalue(self=send))
+    return send
+
 
 #post_user()
 #post_faq()
 #post_poll()
 #post_block()
-faq_list()
+#faq_list()
 #login_token()
+#faq_add()
